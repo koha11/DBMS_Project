69781ef3fa93203ff9bfd121fdf0fd9b78001591
koha11 <110100100+koha11@users.noreply.github.com>
2024-04-07 18:33:49 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -7,7 +7,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="./Css/style.css?v=<? echo time(); ?>">
+    <link rel="stylesheet" href="./Css/style.css">
     <title>Home</title>
 </head>
 
